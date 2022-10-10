27d4a1fe3a3f0ea48965e366f65e6a6072efb991
James Fenn <me@jfenn.me>
2022-10-09 21:32:06 -0400

    add timeout to channel requests

--- src/index.ts
+++ src/index.ts
@@ -13,21 +13,31 @@ app.use(cors({
 	origin: ['https://theframedrops.com', /^http\:\/\/127\.0\.0\.1\:/]
 }));
 
+function resolveTimeout<T>(fun: () => Promise<T>, timeout: number) : Promise<T|null> {
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(null), timeout);
+		fun().then(resolve);
+	});
+}
+
 app.get('/channels/*', async (req, res) => {
 	if (req.headers.origin !== 'https://theframedrops.com' && !req.headers.origin?.startsWith('http://127.0.0.1:')) {
 		res.status(500).send();
-		return;
+		return;3
 	}
 
 	const channels = req.path.split("/").splice(2);
 	const ret: {[channel: string]: ChannelInfo} = {};
 
 	await Promise.all(channels.map(async (channel) => {
-		const info = await getChannelInfo(channel);
+		const info = await resolveTimeout(
+			() => getChannelInfo(channel),
+			1000
+		);
+
 		if (info) ret[channel] = info;
 	}));
 
-	res.setHeader("Cache-Control", "public, max-age=30");
 	res.json(ret);
 });
 
